9fb478efa254a286cc033375dcac9497faa5e717
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-09 16:50:09 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,5 @@
- # Define a local variable called 'greeting',
+def greeting
+puts "Hello World!
+end
+" # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
